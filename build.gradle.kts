7c1d3a879b1dd4ab5461a12d21df90b9cabc6860
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2022-01-13 15:26:51 +0200

    update quality plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    id("ru.vyarus.quality").version("4.5.0")
+    id("ru.vyarus.quality").version("4.7.0")
     id("java-library")
     id("jacoco")
 }
